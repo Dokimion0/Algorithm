c03d1554eefee2fc2bbfc87c98b77b1220e68c89
LCY36 <leecy3610@gmail.com>
2022-09-29 15:59:25 +0900

    10430

---
+++ 입출력과 사칙연산/10430.js
@@ -0,0 +1,8 @@
+var input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+var A = parseInt(input[0]);
+var B = parseInt(input[1]);
+var C = parseInt(input[2]);
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
