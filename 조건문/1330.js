9c3ff89aa2e99cd157a7af82bb589ec895c3b21a
LCY36 <leecy3610@gmail.com>
2022-09-29 16:37:54 +0900

    1330

--- 조건문/1330.js
+++ 조건문/1330.js
@@ -1,6 +1,7 @@
 var input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
-var A = parseInt(input[0]);
-var B = parseInt(input[1]);
-if (A > B) console.log(">");
-else if (A < B) console.log("<");
-else console.log("==");
+var score = parseInt(input[0]);
+if (score >= 90) console.log("A");
+else if (score >= 80) console.log("B");
+else if (score >= 70) console.log("C");
+else if (score >= 60) console.log("D");
+else console.log("F");
