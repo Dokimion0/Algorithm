7e98a241a38dd7d05f09c3411020b99740b38083
LCY36 <leecy3610@gmail.com>
2022-10-01 17:11:51 +0900

    2438

--- 반복문/2438.js
+++ 반복문/2438.js
@@ -3,14 +3,11 @@ let answer = "";
 let max = parseInt(input[0]);
 
 for (let i = 0; i < max; i++) {
-  for (let j = 0; j < max - i-1; j++) {
-    answer += " ";
-  }
-  for (let k = 0; k <= i; k++) {
+  answer += " ";
+  for (let j = 0; j <= i; j++) {
     answer += "*";
   }
   answer += "\n";
 }
 
 console.log(answer);
-
