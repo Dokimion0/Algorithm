a094f428e196252135baef93e789691c84e75c62
lcy36 <leecy3610@gmail.com>
2022-10-26 22:04:43 +0900

    1065

---
+++ 함수/1065.js
@@ -0,0 +1,16 @@
+let input = require("fs").readFileSync("dev/stdin").toString().split("\n");
+let count = 0;
+
+for (let i = 1; i <= Number(input); i++) {
+  let digit = String(i);
+  if (i < 100) {
+    count++;
+  } else if (i < 1000) {
+    let diff1 = Number(digit[0]) - Number(digit[1]);
+    let diff2 = Number(digit[1]) - Number(digit[2]);
+    if (diff1 === diff2) {
+      count++;
+    }
+  }
+}
+console.log(count);
