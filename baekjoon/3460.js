64e103e960de9b7e737083aea8734bc2c1bcf5fc
dokimion24 <leecy3610@gmail.com>
2023-12-14 21:22:49 +0900

    solved

--- baekjoon/3460.js
+++ baekjoon/3460.js
@@ -1,16 +1,19 @@
-let input = require('fs').readFileSync('dev/stdin').toString().split('\n');
-let count = 0;
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n')
+  .map(Number);
 
-for (let i = 1; i <= Number(input); i++) {
-  let digit = String(i);
-  if (i < 100) {
-    count++;
-  } else if (i < 1000) {
-    let diff1 = Number(digit[0]) - Number(digit[1]);
-    let diff2 = Number(digit[1]) - Number(digit[2]);
-    if (diff1 === diff2) {
-      count++;
-    }
-  }
-}
-console.log(count);
+const solution = (input) => {
+  const testArr = input.slice(1);
+  testArr.forEach((item) => {
+    const printArr = [];
+    [...item.toString(2)].reverse().forEach((bit, i) => {
+      if (bit === '1') printArr.push(i);
+    });
+    console.log(printArr.join(' '));
+  });
+};
+
+solution(input);
